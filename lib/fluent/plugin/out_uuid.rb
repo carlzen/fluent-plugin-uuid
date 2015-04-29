139e8c835d7408ea7c568d27745840f1d669bac7
j-o-s-h <joshcwa@live.com>
2015-04-29 16:01:58 -0700

    still fixing

--- lib/fluent/plugin/out_uuid.rb
+++ lib/fluent/plugin/out_uuid.rb
@@ -6,6 +6,7 @@ class Fluent::AddOutput < Fluent::Output
 
   config_param :uuid_len,       :integer, :default => 32
   config_param :uuid_key_name,  :string,  :default => 'uuid'
+  config_param :add_tag_prefix, :string, :default => nil
 
   def initialize
     super
@@ -13,16 +14,28 @@ class Fluent::AddOutput < Fluent::Output
 
   def configure(conf)
     super
+    @tag_prefix = "#{@add_tag_prefix}."
     @add_hash = Hash.new
+
+    @tag_proc =
+      if @tag_prefix
+        Proc.new {|tag| "#{@tag_prefix}#{tag}" }
+      else
+        Proc.new {|tag| tag }
+      end
+
   end
 
   def emit(tag, es, chain)
+    emit_tag = @tag_proc.call(tag)
+
     es.each do |time,record|
       @add_hash.each do |k,v|
         record[k] = v
       end
-      @add_hash[@uuid_key_name] = SecureRandom.hex(@uuid_len)
-      Fluent::Engine.emit(tag, time, record)
+      record[@uuid_key_name] = SecureRandom.hex(@uuid_len)
+
+      Fluent::Engine.emit(emit_tag, time, record)
     end
 
     chain.next
